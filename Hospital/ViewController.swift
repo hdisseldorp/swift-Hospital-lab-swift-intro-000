deff7800c62b49ee6d0e35eeb67217cede9bba07
Hendricus Disseldorp <hdisseldorp@gmail.com>
2017-04-23 12:47:19 +0300

    Did some POP - Protocol Oriented Programming

--- Hospital/ViewController.swift
+++ Hospital/ViewController.swift
@@ -8,12 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        
-        
-    }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
     
+    let advisoryBM = AdvisoryBoardMember(name: "Board Member", vacationDays: 25)
+    let principal = Principal(name: "Principal", vacationDays: 22)
+    let teacher = Teacher(name: "Teacher", vacationDays: 30)
+    
+    print("Advisory Board Member: \(advisoryBM.name) \(advisoryBM.wages()), \(advisoryBM.vacationDays)")
+    print("Principal            : \(principal.name) \(principal.wages()), \(advisoryBM.vacationDays)")
+    print("Teacher              : \(teacher.name) \(teacher.wages()), \(teacher.vacationDays)")
+  }
+  
 }
